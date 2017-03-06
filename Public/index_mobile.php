8af1539d8d79b1979d73204e9e9c1314ce563184
Stevens (Home <stevensc92@gmail.com>
2017-03-06 01:13:47 +0100

    update mobile version 0.2'

--- Public/index_mobile.php
+++ Public/index_mobile.php
@@ -7,14 +7,14 @@
 		<meta name="description" content="L’Ecole d’Equitation Ledermann près d’Evreux en Normandie vous propose promenades à cheval, leçons, pensions, CSO. Tous niveaux du débutant au confirmé, de la balade à la haute compétition."/>
 		<meta name="robots" content="index, follow" />
 		<meta http-equiv="content-language" content="fr" />
-		<meta property="og:image" content="http://equitationledermann.com/design/img/1.png" />
+		<meta property="og:image" content="http://equitationledermann.com/Public/img/alexandra_saut.png" />
 
-		<link rel="stylesheet" href="design/css/reset.css" />
-		<link rel="stylesheet" href="design/css/bootstrap.min.css" type="text/css" media="all" />
-		<!-- <link rel="stylesheet" href="design/css/main.css" type="text/css" media="all" />
-		<link rel="stylesheet" href="design/css/style.css" type="text/css" media="all" />
-		<link rel="stylesheet" href="design/css/style_css-inline.css" type="text/css" > -->
-		<link rel="stylesheet" href="mobile.css" type="text/css">
+		<!-- <link rel="stylesheet" href="./css/reset.css" /> -->
+		<link rel="stylesheet" href="./css/bootstrap.min.css" type="text/css" media="all" />
+		<!-- <link rel="stylesheet" href="./css/main.css" type="text/css" media="all" />
+		<link rel="stylesheet" href="./css/style.css" type="text/css" media="all" />
+		<link rel="stylesheet" href="./css/style_css-inline.css" type="text/css" > -->
+		<link rel="stylesheet" href="./css/mobile.css" type="text/css">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css" type="text/css">
 
 		<title>Ecole d'Equitation Ledermann - Evreux</title>
@@ -27,30 +27,30 @@
 	<!-- == HEADER == -->
 
 	<header id="header">
-		<a href=""> <img src="design/img/logo.png" id="header-logo"/> </a>
+		<a href=""> <img src="./img/logo.png" id="header-logo"/> </a>
 	</header>
 
 	<main id="container">
 		<div id="home">
-			<img src="design/img/ecole.jpg" class="img-responsive" />
+			<img src="./img/ecole.jpg" class="image-background" />
 		</div>
 
 		<div class="container" id="school">
 			<div class="row">
 				<div class="title-section">
-					<h3 class="title">L'Ecole d'Equitation Ledermann</h3>
+					<h1 class="title">L'Ecole d'Equitation Ledermann</h1>
 					<div class="sub-title">De la balade en forêt au plus haut niveau de compétition</div>
 				</div>
 
-				<div class="col-xs-12 col-sm-12 img-center">
+				<div class="col-xs-12 col-sm-12 col-md-2 img-center">
 					<img
 						class="img-mobile"
-						src="design/img/balade.jpg"
+						src="./img/balade.jpg"
 						alt="Balade à cheval"
 						title="Balade à cheval"
 					/>
 				</div>
-				<div class="col-xs-12 col-sm-12">
+				<div class="col-xs-12 col-sm-12 col-md-4">
 					<p class="img-text">
 						Que vous souhaitiez découvrir les joies de<br>
 						l'Equitation en toute sécurité, aller plus loin<br>
@@ -61,17 +61,17 @@
 						répondre à vos attentes.
 					</p>
 				</div>
-				
-				<div class="col-xs-12 col-sm-12 img-center">
+
+				<div class="col-xs-12 col-sm-12 col-md-2 col-md-push-4 img-center">
 					<img
 						class="img-mobile"
-						src="design/img/podium.png"
+						src="./img/podium.png"
 						alt="Podium"
 						title="Podium"
 					/>
 				</div>
 
-				<div class="col-xs-12 col-sm-12">
+				<div class="col-xs-12 col-sm-12 col-md-4 col-md-pull-2">
 					<p class="img-text">
 						L'Ecole d'Equitation Ledermann<br>
 						a formé plusieurs champions,<br>
@@ -79,6 +79,72 @@
 						médaille de bronze en individuel aux<br>
 						Jeux Olympiques d'Atlanta et 1ère femme<br>
 						Championne d'Europe de Saut d'Obstacles.
+					</p>
+				</div>
+			</div>
+		</div>
+
+		<div id="portfolio">
+			<img src="./img/alexandra_saut.png" class="image-background" />
+		</div>
+
+		<div class="container" id="philosophie">
+			<div class="row">
+				<div class="title-section">
+					<h1 class="title">Notre philosophie</h1>
+					<div class="sub-title">Sérieux et professionel</div>
+				</div>
+			</div>
+
+			<div class="row col-md-4">
+				<div class="col-xs-12 col-sm-12 img-center">
+					<img
+						class="img-mobile"
+						src="./img/terrain_entrainement.png"
+						alt="Terrain d'entraînement"
+					/>
+				</div>
+
+				<div class="col-xs-12 col-sm-12">
+					<p class="img-text">
+						Nous proposons à tous un enseignement à la fois classique et attractif qui
+						permet de monter en toute sécurité et avec plaisir aussi bien en promenade
+						qu’en compétition.
+					</p>
+				</div>
+			</div>
+
+			<div class="row col-md-4">
+				<div class="col-xs-12 col-sm-12 img-center">
+					<img
+						class="img-mobile"
+						src="./img/alexandra_cheval.png"
+						alt="Alexandra et son cheval"
+					/>
+				</div>
+
+				<div class="col-xs-12 col-sm-12">
+					<p class="img-text">
+						Nos priorités : la mise en confiance du cavalier et le respect du cheval.
+						Nos buts : instaurer entre vous et le cheval une relation basée sur
+						la confiance, la complicité et le plaisir partagé.
+					</p>
+				</div>
+			</div>
+
+			<div class="row col-md-4">
+				<div class="col-xs-12 col-sm-12 img-center">
+					<img
+						class="img-mobile"
+						src="./img/ecole_detente.png"
+						alt="Ecole détente"
+					/>
+				</div>
+
+				<div class="col-xs-12 col-sm-12">
+					<p class="img-text">
+						Venez nous voir, nous nous ferons un plaisir de vous faire découvrir ce que
+						nous pratiquons et enseignons depuis 50 ans.
 					</p>
 				</div>
 			</div>
@@ -98,7 +164,7 @@
 
 		</script>
 		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-		<script type="text/javascript" src="js/app.js"></script>
+		<!-- <script type="text/javascript" src="js/app.js"></script> -->
 		<!-- <script type="text/javascript" src="http://parasponsive.com/para1/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.0"></script> -->
 
 	</footer>
