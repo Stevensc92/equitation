02dd141ce874208368b832e5cebe4e6e4e4b5c4f
Stevens (Home <stevensc92@gmail.com>
2017-03-11 17:32:57 +0100

    update responsive version v0.9

---
+++ Public/old_index.php
@@ -0,0 +1,409 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8"/>
+		<meta name="viewport" content="width=device-width, height=device-height, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
+		<meta name="keywords" lang="fr" content="Equitation Evreux, Equitation Eure, Equitation Normandie, Centre Equestre Evreux, Centre Equestre Eure, Centre Equestre Normandie, Club Hippique Evreux, Club Hippique Eure, Club Hippique Normandie, Balade cheval Evreux, Balade cheval Eure, Balade cheval Normandie, Promenade cheval Evreux, Promenade cheval Eure, Promenade cheval Normandie, Pension cheval Evreux, Pension cheval Eure, Pension cheval Normandie, Pension cheval région parisienne, Débourrage cheval Evreux, Débourrage cheval Eure, Débourrage cheval Normandie, Stage CSO Evreux, Stage CSO Eure, Stage CSO Normandie, Stage CSO Ledermann, Equitation Ledermann, Alexandra Ledermann, Cheval Evreux, Cheval Eure, Cheval Normandie"/>
+		<meta name="description" content="L’Ecole d’Equitation Ledermann près d’Evreux en Normandie vous propose promenades à cheval, leçons, pensions, CSO. Tous niveaux du débutant au confirmé, de la balade à la haute compétition."/>
+		<meta name="robots" content="index, follow" />
+		<meta http-equiv="content-language" content="fr" />
+		<meta property="og:image" content="http://equitationledermann.com/Public/img/alexandra_saut.png" />
+		<meta name="google-site-verification" content="dJ3CQa5sUEguIoU3GPJ6sGkV8VxzMEjf04xp-BKbhQY" />
+
+		<link rel="stylesheet" href="./css/bootstrap.min.css" type="text/css" media="all" />
+		<link rel="stylesheet" href="./css/main.css" type="text/css" media="all" />
+		<link rel="stylesheet" href="./css/style_css-inline.css" type="text/css" >
+		<link rel="stylesheet" href="./css/app.css" type="text/css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css" type="text/css">
+
+		<title>Ecole d'Equitation Ledermann - Evreux</title>
+	</head>
+
+	<body>
+
+
+
+<!-- == HEADER == -->
+
+<header>
+	<div class="logo">
+		<a href="">
+			<img
+				class="img-responsive"
+				src="./img/logo.png"
+				alt="Logo"
+				title="Logo école d'équitation"
+			/>
+		</a>
+	</div>
+
+	<div class="container">
+		<div id="navbar" class="navbar-collapse collapse">
+			<ul class="nav navbar-nav">
+				<li><a href="#anc-famille" class="ancre">L'histoire</a></li>
+				<li><a href="#anc-philosophie" class="ancre">Philosophie</a></li>
+				<li><a href="#installation" class="ancre">Installations</a></li>
+				<li><a href="#anc-service" class="ancre">Services</a></li>
+				<li><a href="#anc-tarif" class="ancre">Tarifs</a></li>
+				<li><a href="#evreux" class="ancre">Localisation</a></li>
+				<li>
+					<a href="#evreux" class="ancre">
+						<i class="fa fa-envelope-o" aria-hidden="true"></i>
+					</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</header>
+
+
+<!-- ========================= SECTIONS ============================= -->
+<div id="pages" class="intro-off">
+    <ul class="nostyle">
+
+        <!-- ========== HOME SECTION ========== -->
+<li id="home" class=" slider-on">
+   <div class="ecole">
+   </div>
+</li>
+
+<!-- ========== SERVICES SECTION ========== -->
+<li id="school" class="section2">
+	<section class="school_box man_box">
+		<div class="school_top tbox-grey"></div>
+		<div class="school_mid bg-grey">
+			<div class="container text-center">
+
+				<div class="row title_section">
+					<h3 class="text_center">L'Ecole d'Equitation Ledermann</h3>
+					<div class="lines sub_title text_center"><p>De la balade en forêt au plus haut niveau de compétition</p></div>
+				</div>
+
+				<div class="row">
+					<div class="col-md-2">
+						<div class="intro_img fleft">
+							<img
+								class="img-responsive"
+								src="./img/balade.jpg"
+								alt="Balade à cheval"
+								title="Balade à cheval"
+							/>
+						</div>
+					</div>
+
+					<div class="col-md-4">
+						<div class="text">
+							<p>
+								Que vous souhaitiez découvrir les joies de<br>
+								l'Equitation en toute sécurité, aller plus loin<br>
+								dans la connaissance du cheval, ou encore<br>
+								améliorer vos performances en compétition,<br>
+								vous trouverez un accompagnement<br>
+								personnalisé de qualité qui saura<br>
+								répondre à vos attentes.
+							</p>
+						</div>
+					</div>
+
+					<div class="col-md-4">
+						<div class="text">
+							<p>
+								L'Ecole d'Equitation Ledermann<br>
+								a formé plusieurs champions,<br>
+								notamment Alexandra Ledermann,<br>
+								médaille de bronze en individuel aux<br>
+								Jeux Olympiques d'Atlanta et 1ère femme<br>
+								Championne d'Europe de Saut d'Obstacles.
+							</p>
+						</div>
+					</div>
+
+					<div class="col-md-2">
+						<div class="intro_img fright">
+							<img
+								class="img-responsive"
+								src="./img/podium.png"
+								alt="Podium"
+								title="Podium"
+							/>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="school_bot bbox-grey"></div>
+	</section>
+</li>
+
+<li id="portfolio" class="section3">
+</li>
+
+<li id="famille" class="section4">
+	<section id="anc-famille" class="famille_main man_box">
+		<div class="famille_top"></div>
+		<div class="famille_mid mid_box">
+			<div class="container text-center">
+				<h3 class="text_center">L'histoire</h3>
+				<div class="row">
+					<div class="item col-md-4">
+						<div class="item_p portrait">
+							<img src="./img/grand_pere_cheval.jpg" class="img-responsive" alt="Grand père sur un cheval"/>
+						</div>
+					</div>
+					<div class="item col-md-4">
+						<div class="item_p" style="margin-top:35px;">
+							<img src="./img/saut_obstacle.jpg" class="img-responsive" alt="Saut d'obstacle" />
+						</div>
+					</div>
+					<div class="item col-md-4">
+						<div class="item_p portrait">
+							<img src="./img/alexandra_celebration.jpg" class="img-responsive" alt="Alexandre Ledermann" />
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12">
+						<div class="text">
+							<p>Près de 50 ans séparent ces images, du grand-père Albert pionnier des centres équestres en France,<br>
+							à la médaillée	olympique Alexandra en passant par
+							son père Jean-Pierre, cavalier de CSO émérite<br>
+							et fondateur en 1966 de l'Ecole d'Equitation Ledermann à Huest.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</li>
+
+<li id="philosophie" class="section5">
+	<section id="anc-philosophie" class="philosophie_main man_box">
+		<div class="philosophie_top "></div>
+		<div class="philosophie_mid mid_box">
+			<div class="container text-center">
+				<h3 class="text_center">Notre philosophie</h3>
+
+				<div class="row">
+					<div class="item col-md-4">
+						<div class="item_p">
+							<img src="./img/terrain_entrainement.png" class="img-responsive" alt="Terrain d'entraînement" />
+						</div>
+						<div class="text">
+							<p>Nous proposons à tous un enseignement à la fois classique et attractif qui permet de monter en toute sécurité et avec plaisir aussi bien en promenade qu’en compétition.</p>
+						</div>
+					</div>
+					<div class="item col-md-4">
+						<div class="item_p">
+							<img src="./img/alexandra_cheval.png" class="img-responsive" alt="Alexandra et son cheval" />
+						</div>
+						<div class="text">
+							<p>Nos priorités : la mise en confiance du cavalier et le respect du cheval.<br>
+	            			Nos buts : instaurer entre vous et le cheval une relation basée sur la confiance, la complicité et le plaisir partagé.</p>
+						</div>
+					</div>
+					<div class="item col-md-4">
+						<div class="item_p">
+							<img src="./img/ecole_detente.png" class="img-responsive" alt="Ecole détente" />
+						</div>
+						<div class="text">
+							<p>Venez nous voir, nous nous ferons un plaisir de vous faire découvrir ce que nous pratiquons et enseignons depuis 50 ans.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</li>
+
+<li id="about_us" class="section6">
+</li>
+
+<li id="installation" class="section7">
+	<section class="installation_main man_box">
+		<div class="installation_top "></div>
+		<div class="installation_mid mid_box">
+			<div class="container text-center">
+				<h3 class="text_center">Les installations</h3>
+				<div class="lines sub_title text_center"></div>
+				<div class="row">
+					<div class="item_p">
+            <img
+              class="img-responsive"
+              src="./img/terrain_exterieur_1.png"
+              alt="Terrain extérieur"
+            />
+          </div>
+
+					<div class="item_p">
+            <img
+              class="img-responsive"
+              src="./img/ecurie_exterieur.png"
+              alt="Terrain écurie"
+            />
+          </div><br>
+					<div class="item_p">
+            <img
+              class="img-responsive"
+              src="./img/terrain_exterieur_2.png"
+              alt="Terrain extérieur"
+            />
+          </div>
+					<div class="item_p">
+            <img
+              class="img-responsive"
+              src="./img/ecurie_interieur.png"
+              alt="Ecurie interieur"
+            />
+          </div>
+				</div>
+
+				<div class="row">
+					<div class="col-md-12 text">
+						<p>L'Ecole d'Equitation Ledermann dispose d'une quarantaine de boxes, d'un<br>
+						manège couvert, de plusieurs paddocks et de 2 carrières dont une éclairée et<br>
+						un vaste spring-garden aux abords de la forêt<br>
+						comprenant un terrain en herbe et un parcours de cross.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</li>
+
+<li id="woo" class="section8">
+</li>
+
+<li id="service" class="section9">
+	<section id="anc-service" class="service_main man_box">
+		<div class="service_top"></div>
+		<div class="service_mid mid_box">
+			<div class="container text-center">
+				<h3 class="text_center">Les services</h3>
+				<div class="row">
+					<div class="item col-md-4">
+						<div class="item_p">
+							<img src="./img/terrain_obstacle.png" alt="Terrain obstacle" class="img-responsive">
+						</div>
+						<div class="text">
+							<span class="bold">Cours d'Equitation du débutant au confirmé à partir de 8 ans</span><br><br>
+							<span class="bold">Promenades</span>
+						</div>
+					</div>
+					<div class="item col-md-4">
+						<div class="item_p">
+							<img src="./img/terrain_obstacle_2.png" alt="Terrain obstacle" class="img-responsive">
+						</div>
+						<div class="text">
+							<span class="bold">Travail et sortie en concours de votre cheval</span><br>
+							(Nationaux et Cycle Classique Jeunes Chevaux).<br><br>
+							<span class="bold">Coaching CSO personnalisé</span><br>
+						</div>
+					</div>
+					<div class="item col-md-4">
+						<div class="item_p">
+							<img src="./img/ecurie_cheval.png" alt="" class="img-responsive">
+						</div>
+						<div class="text">
+							<span class="bold">Pension pour votre cheval</span><br>
+							de loisir ou de sport <br>
+							(à la carte selon vos besoins)<br><br>
+							<span class="bold">Débourrage du jeune cheval</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</li>
+
+
+<li id="facade" class="section10">
+</li>
+
+<li id="tarif" class="section11">
+	<section id="anc-tarif" class="tarif_main man_box">
+		<div class="tarif_top"></div>
+		<div class="tarif_mid mid_box">
+			<div class="container text-center">
+				<h3 class="text_center">Les tarifs</h3>
+				<div class="row">
+					<div class="col-md-6">
+						<ul class="tarif_ul">
+							<li>Leçons et balades à partir de : <br><span class="bold">22 € / l'heure</span> (tarif adhérent)<br>
+							<span class="bold">36 € / l'heure</span> (tarif non adhérent) </li>
+							<li>Pensions à partir de : <br><span class="bold">590 € / mois</span></li>
+							<li>Coaching CSO personnalisé avec <br><span class="bold">Jean-Pierre ou Alexandra Ledermann</span> :<br> nous consulter</li>
+						</ul>
+					</div>
+					<div class="col-md-1"></div>
+					<div class="col-md-4 tarif_img">
+						<img src="./img/cheval_bisou.png" alt="Bisous de cheval" class="img-responsive">
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</li>
+
+<li id="evreux" class="section12">
+	<section class="contact_main man_box">
+		<div class="contact_top"></div>
+		<div class="contact_mid mid_box">
+			<div class="container text-center">
+				<h3 class="text_center">Contact et localisation</h3>
+				<div class="row">
+					<div class="col-md-5">
+						<div class="info_trajet txt" style="text-transform:none;">
+							<img src="./img/localisation.jpg" class="img-responsive" style="display:block; margin:auto;padding-bottom:30px;" alt="Vue aérienne" />
+							<img src="./img/feets.jpg" class="img-responsive" style="margin:auto;" alt="Feet" />
+							A 5 mn d'Evreux<br>
+							30 mn de Rouen<br>
+							50 mn de Paris Porte Maillot<br>
+						</div>
+					</div>
+					<div class="col-md-6">
+					<img src="./img/maps.jpg" class="img-responsive" alt="Map équitation Evreux" />
+						<!-- <div class="maps"></div> -->
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-4"></div>
+					<div class="col-md-8">
+						<div class="sub_maps">
+							<span class="localisation">
+								<img src="./img/icone_localisation.png" alt="Localisation" /> <span class="txt">2 Rue de Fauville<br> 27930 HUEST</span>
+							</span>
+							<span class="mail">
+								<i class="fa fa-envelope-o" aria-hidden="true" style="font-size:20px; float:left; margin-top:15px;"></i>
+								<span class="txt">&nbsp;ecurieledermann@alicepro.fr</span>
+							</span>
+							<span class="number">
+								<img src="./img/icone_tel.png" alt="Telephone" />
+								<span class="txt">Tél : 02 27 34 17 42</span>
+							</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</li>
+
+	</ul>
+</div>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-91585223-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+<!-- <script type="text/javascript" src="js/app.js"></script> -->
+<!-- <script type="text/javascript" src="http://parasponsive.com/para1/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.0"></script> -->
+
+</body>
+</html>
