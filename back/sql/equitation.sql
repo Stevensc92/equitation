53be48bf8cf5048fcd07a4c83ee337ea55703e20
Stevens (Home <stevensc92@gmail.com>
2017-04-21 03:09:33 +0200

    update databaseg

--- back/sql/equitation.sql
+++ back/sql/equitation.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Client :  127.0.0.1
--- Généré le :  Mar 04 Avril 2017 à 21:29
+-- Généré le :  Ven 21 Avril 2017 à 01:09
 -- Version du serveur :  5.7.14
--- Version de PHP :  5.6.25
+-- Version de PHP :  7.0.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
